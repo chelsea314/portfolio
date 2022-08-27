b764847e4d71eca6d32beba6f16734d48feeba51
Chelsea Sexton <chelseansexton@gmail.com>
2022-08-27 17:05:05 -0500

    styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import '../../styles/About.css';
+
+
 export default function About() {
     return (
-    <div>
-        <h1>About Me</h1>
-        <p>This is about me.</p>
-        {/* <img src="../"></img> */}
+    <div className="about">
+        <div className="container mt-3">
+            <div className="row">
+                <div className = "col-12 col-md-9">
+                    <h1>About Me</h1>
+                    <p>This is about me.</p>
+                </div>
+                <div className= "col-12 col-md-3">
+                    <img src="chelseaheadshot.png" alt="Chelsea's Headshot" width="300" height="350"/>
+                </div>
+            </div>
+        </div>
     </div>
     );
 }
